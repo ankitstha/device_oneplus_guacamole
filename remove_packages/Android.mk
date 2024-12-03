402fd48ca3019d6c8d51ad517f874290a4748289
Codecity001 <sanganiharish263@gmail.com>
2024-12-03 00:32:54 -0600

    remove_packages: Add more packages
    Signed-off-by: Codecity001 <sanganiharish263@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos YouTube Maps Drive Ornament Velvet
+LOCAL_OVERRIDES_PACKAGES := RecorderPrebuilt Photos YouTube Maps Drive Ornament Velvet Gmail2 PersonalSafety FossifyGallery FilesGoogle Chrome64 CalendarGoogle CalculatorGoogle
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
