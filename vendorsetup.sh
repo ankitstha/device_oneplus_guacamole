671b4bc289c37ea668b5afb5c99ca14220155321
ankitstha <ankitshrestha114@gmail.com>
2025-03-08 12:58:22 +1100

    Add vendorsetup

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/ankitstha/device_oneplus_sm8150-common -b mist device/oneplus/sm8150-common
+git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_guacamole vendor/oneplus/guacamole --depth=1
+git clone https://github.com/ankitstha/vendor_oneplus_sm8150-common.git -b fifteen vendor/oneplus/sm8150-common --depth=1
+git clone https://github.com/Codecity001/android_kernel_oneplus_sm8150.git kernel/oneplus/sm8150 --depth=1
+git clone https://github.com/QuinceROMs/hardware_oneplus-1/commits/15-d-pixelage/ hardware/oplus
+git clone https://github.com/Oneloutre/vendor_Camera.git -b OnePlus7 vendor/Camera/OnePlus7
